dc5aead0409843b54f34151a379ba8bba52a8292
Jason Burns <67003071+Jasonburns234@users.noreply.github.com>
2023-07-29 10:06:19 +0100

    files uploaded

---
+++ index.js
@@ -0,0 +1,138 @@
+// Import Firebase modules from CDN
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import { getDatabase, ref, push, onValue, remove, set } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+
+// Configure Firebase app settings (currently empty, but should contain databaseURL)
+const appSettings = {
+    databaseURL: "https://we-are-the-champions-85a85-default-rtdb.europe-west1.firebasedatabase.app/"
+};
+
+// Initialize the Firebase app with the provided settings
+const app = initializeApp(appSettings);
+
+// Get a reference to the Firebase database instance
+const database = getDatabase(app);
+
+// Get references to HTML elements in the DOM
+const inputFieldEl = document.getElementById("input-field");
+const addButtonEl = document.getElementById("add-button");
+const shoppingListEl = document.getElementById("shopping-list");
+const toFieldEl = document.getElementById("to-field");
+const fromFieldEl = document.getElementById("from-field");
+
+// Add a click event listener to the "Add" button
+addButtonEl.addEventListener("click", function () {
+    // Retrieve the values from the input fields
+    const toValue = toFieldEl.value;
+    const inputValue = inputFieldEl.value;
+    const fromValue = fromFieldEl.value;
+
+    // Create an object containing the "to", "input", "from", and "likes" values
+    const newItem = {
+        to: toValue,
+        input: inputValue,
+        from: fromValue,
+        likes: 0, // Initialize likes count to 0 for new items
+    };
+
+    // Push the new item object to the "shoppingList" location in the database
+    push(ref(database, "shoppingList"), newItem);
+
+    // Clear the input fields after adding the item
+    clearInputFields();
+
+    // Add the heart icon under the "From" paragraph
+    appendHeartIcon();
+});
+
+// Function to clear the content of the input fields
+function clearInputFields() {
+    toFieldEl.value = "";
+    inputFieldEl.value = "";
+    fromFieldEl.value = "";
+}
+
+// Function to append the heart icon under the "From" paragraph
+function appendHeartIcon() {
+    // Create the heart icon element
+    const heartIcon = document.createElement("i");
+    heartIcon.className = "fa fa-heart";
+    heartIcon.style.fontSize = "24px";
+
+    // Append the heart icon under the "From" paragraph
+    fromFieldEl.parentNode.appendChild(heartIcon);
+}
+
+// Add a listener to the "shoppingList" location in the database
+onValue(ref(database, "shoppingList"), function (snapshot) {
+    const items = snapshot.val();
+    if (items) {
+        const itemsArray = Object.entries(items);
+        clearShoppingListEl();
+        itemsArray.forEach(([itemID, item]) => {
+            appendItemToShoppingListEl(itemID, item);
+        });
+    } else {
+        shoppingListEl.innerHTML = "No items here... yet";
+    }
+});
+
+// Function to append a new item to the shopping list element
+function appendItemToShoppingListEl(itemID, item) {
+    const { to, input, from, likes } = item;
+
+    // Create a new list item element
+    const newEl = document.createElement("div");
+    newEl.setAttribute("id", "endorsedContainer");
+
+    // Create and append the "to" paragraph element
+    const toParagraph = document.createElement("p");
+    toParagraph.textContent = `To: ${to}`;
+    newEl.appendChild(toParagraph);
+
+    // Create and append the input paragraph element
+    const inputParagraph = document.createElement("p");
+    inputParagraph.textContent = input;
+    inputParagraph.setAttribute("id", "inputParagraph");
+    newEl.appendChild(inputParagraph);
+
+    // Create and append the "from" paragraph element
+    const fromParagraph = document.createElement("p");
+    fromParagraph.textContent = `From: ${from}`;
+    newEl.appendChild(fromParagraph);
+
+    // Create and append the "Likes" paragraph element
+    const likesParagraph = document.createElement("p");
+    likesParagraph.innerHTML = `Likes: ${likes} <i class="fa fa-heart" style="font-size:14px; color: #28A9F1;"></i>`;
+    likesParagraph.style.textAlign = "right";
+
+    // Add a click event listener to the new element to handle item likes
+    let likeCounter = likes || 0;
+    newEl.addEventListener("click", function () {
+        likeCounter++;
+        likesParagraph.innerHTML = `Likes: ${likeCounter} <i class="fa fa-heart" style="font-size:14px; color: #28A9F1;"></i>`;
+        updateLikeCount(itemID, likeCounter);
+    });
+
+    // Append the "Likes" paragraph to the new element
+    newEl.appendChild(likesParagraph);
+
+    // Add a double-click event listener to the new element to handle item removal
+    newEl.addEventListener("dblclick", function () {
+        const exactLocationOfItemInDB = ref(database, `shoppingList/${itemID}`);
+        remove(exactLocationOfItemInDB);
+    });
+
+    // Append the new element to the shopping list element in the DOM
+    shoppingListEl.append(newEl);
+}
+
+// Function to clear the content of the shopping list element
+function clearShoppingListEl() {
+    shoppingListEl.innerHTML = "";
+}
+
+// Function to update the like count in the database
+function updateLikeCount(itemID, likesCount) {
+    set(ref(database, `shoppingList/${itemID}/likes`), likesCount);
+}
